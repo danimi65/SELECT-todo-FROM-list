8fe098cabebf100df48931fbc8a2cc919ac0c591
Danika Harada <danimi65@gmail.com>
2017-01-19 20:08:38 -1000

    completed

--- todo_app.sql
+++ todo_app.sql
@@ -34,4 +34,24 @@ SELECT "title" FROM "tasks" WHERE "completed_at" IS NULL;
 
 UPDATE "tasks" SET "completed_at" = now() WHERE "title" = 'Study SQL';
 
+SELECT "title", "description" FROM "tasks" WHERE "completed_at" IS NULL;
+
+SELECT * FROM "tasks" ORDER BY "created_at" DESC;
+
+INSERT INTO "tasks" (title, description) VALUES ('mistake 1', 'another test entry');
+
+INSERT INTO "tasks" (title, description) VALUES ('mistake 2', 'another test entry');
+
+INSERT INTO "tasks" (title, description) VALUES ('third mistake', ' another test entry');
+
+SELECT * FROM "tasks" WHERE "title" LIKE '%mistake%';
+
+DELETE FROM "tasks" WHERE "title" = 'mistake 1';
+
+SELECT "title", "description" FROM "tasks" WHERE "title" LIKE '%mistake%';
+
+DELETE FROM "tasks" WHERE "title" LIKE '%mistake%';
+
+SELECT * FROM "tasks" ORDER BY "title" ASC;
+
 SELECT * FROM "tasks";
